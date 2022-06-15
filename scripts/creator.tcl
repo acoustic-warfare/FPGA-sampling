7ed3ed171c3519000a58b977d58dc3514d8195ed
JacobDrotz <jacobdrotz@live.se>
2022-06-15 16:40:57 +0200

    städning

--- scripts/creator.tcl
+++ scripts/creator.tcl
@@ -1,11 +1,11 @@
 # Typical usage: vivado -mode tcl -source run_bft_project.tcl
 # Create the project and directory structure
-create_project -force ../project ./project -part xc7z020clg400-1
+create_project -force ../vivado_files/project ./project -part xc7z020clg400-1
 #
 # Add various sources to the project
 add_files ../src/fifo_test.vhd\
 
-add_files -fileset sim_1 ../test/tb_fifo_test.vhd
+#add_files -fileset sim_1 ../test/tb_fifo_test.vhd
 #add_files ./Sources/hdl/bftLib/
 add_files -fileset constrs_1 ../src/sync_fifo_constraint.xdc
 #
