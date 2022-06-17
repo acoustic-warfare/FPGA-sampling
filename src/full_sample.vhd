0fe570af0c7f24ca90c4de514169a632b40a604f
JacobDrotz <jacobdrotz@live.se>
2022-06-17 14:44:44 +0200

    added full_sample test code

---
+++ src/full_sample.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use work.MATRIX_TYPE.all;
+
+entity full_sample is
+    generic(
+            bits_mic : integer := 24;
+            nr_mics  : integer := 63
+    );
+
+    Port (
+        clk     : in std_logic;
+        sample_out_matrix : out SAMPLE_MATRIX
+
+
+    );
+end full_sample;
+
+
+architecture behavroal of full_sample is
+
+    signal tmp_vector : std_logic_vector(bits_mic-1 downto 0);
+    signal counter_col : integer := 0;
+    signal counter_row : integer := 0;
+
+begin
+
+    create_sample_matrix : process(clk)
+    begin
+        for i in 0 to 15 loop
+            sample_out_matrix(i) <= data_out_matrix_1(i);
+        end loop;
+
+        for i in 16 to 31 loop
+            sample_out_matrix(i) <= data_out_matrix_2(i);
+        end loop;
+
+        for i in 32 to 47 loop
+            sample_out_matrix(i) <= data_out_matrix_3(i);
+        end loop;
+
+        for i in 48 to 63 loop
+            sample_out_matrix(i) <= data_out_matrix_4(i);
+        end loop;
+
+
+    end process;
+end behavroal;
--- src/matrix_package.vhd
+++ src/matrix_package.vhd
@@ -5,4 +5,5 @@ use IEEE.NUMERIC_STD.ALL;
 
 package MATRIX_TYPE is
    type MATRIX is array (0 to 15) of std_logic_vector(23 downto 0);  --declares matrix of 16 rowns with vectors of 24 bits
+   type SAMPLE_MATRIX is array (0 to 63) of std_logic_vector(23 downto 0);  --declares matrix of 16 rowns with vectors of 24 bits
 end package MATRIX_TYPE;
