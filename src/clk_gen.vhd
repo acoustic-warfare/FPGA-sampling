f18e916643ee176b2ddf499f5f4738eb312692cb
JacobDrotz <jacobdrotz@live.se>
2022-06-20 16:54:08 +0200

    added clk_gen test

---
+++ src/clk_gen.vhd
@@ -0,0 +1,58 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_gen is
+
+    Port (clk : in std_logic;
+         fsck_clk : out std_logic := '0';
+         fs_clk   : out std_logic := '0'
+    );
+
+end clk_gen;
+
+architecture Behavioral of clk_gen is
+    signal rising_edge_counter : integer := 0;
+
+    --procedure clk_div ( clk : in std_logic;
+    --                signal clk_half : out std_logic) is
+        --begin
+        --  if(rising_edge(clk)) then
+         --    clk_half <= not clk;
+        --end if;
+    --end procedure;
+
+begin
+
+    fsck_clk_gen : process(clk)
+    begin
+        if(rising_edge(clk)) then
+            fsck_clk <= not fsck_clk;
+            rising_edge_counter <= rising_edge_counter +1;
+        elsif(rising_edge_counter = 1024) then
+            rising_edge_counter <= 0;
+        end if;
+
+    end process;
+
+    fs_clk_gen: process(clk)
+    begin
+        if(rising_edge(clk) and rising_edge_counter = 1023) then
+            fs_clk <= not fs_clk;
+
+
+
+        end if;
+    end process;
+
+
+    counter_reset: process(rising_edge_counter)
+    begin
+        if(rising_edge_counter = 1024) then
+           -- rising_edge_counter <= 0;
+        end if;
+    end process;
+
+
+
+
+end Behavioral;
---
+++ test/tb_clk_gen.vhd
@@ -0,0 +1,80 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+LIBRARY vunit_lib;
+CONTEXT vunit_lib.vunit_context;
+
+USE work.MATRIX_TYPE.ALL;
+
+ENTITY tb_clk_gen IS
+    GENERIC (
+        runner_cfg : STRING
+    );
+END tb_clk_gen;
+
+ARCHITECTURE tb OF tb_clk_gen  IS
+    CONSTANT clk_cykle : TIME := 10 ns;
+    SIGNAL nr_clk : INTEGER := 0; --not yet in use
+
+    COMPONENT clk_gen
+    Port (clk : in std_logic;
+          fsck_clk : out std_logic;
+          fs_clk   : out std_logic
+          );
+    END COMPONENT;
+
+    SIGNAL clk : STD_LOGIC := '0';
+    SIGNAL reset : STD_LOGIC;
+    SIGNAL fsck_clk : STD_LOGIC;
+    SIGNAL fs_clk : STD_LOGIC;
+
+
+
+BEGIN
+
+    CLK_GEN1 : clk_gen PORT MAP(
+        clk => clk,
+        fsck_clk => fsck_clk,
+        fs_clk => fs_clk
+    );
+
+    clock : PROCESS
+    BEGIN
+        clk <= '0';
+        WAIT FOR clk_cykle/2;
+        clk <= '1';
+        WAIT FOR clk_cykle/2;
+        nr_clk <= nr_clk + 1;
+    END PROCESS;
+
+
+    main : PROCESS
+    BEGIN
+
+
+
+        test_runner_setup(runner, runner_cfg);
+        WHILE test_suite LOOP
+            IF run("tb_clk_1") THEN
+
+                 WAIT FOR 30000 ns;
+
+
+
+                check(1 = 1, "test_1");
+
+
+            ELSIF run("tb_clk_2") THEN
+
+                check(1 = 1, "test_1");
+
+                WAIT FOR 11 ns;
+
+            END IF;
+        END LOOP;
+
+        test_runner_cleanup(runner);
+    END PROCESS;
+
+    test_runner_watchdog(runner, 100 ms);
+END ARCHITECTURE;
