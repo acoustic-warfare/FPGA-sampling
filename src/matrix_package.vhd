1ce41499c8e6c81c21ea90b32b0b8223ccaefd83
JacobDrotz <jacobdrotz@live.se>
2022-06-21 13:14:46 +0200

    comments for full_sample

--- src/matrix_package.vhd
+++ src/matrix_package.vhd
@@ -3,5 +3,5 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 package MATRIX_TYPE is
    type MATRIX is array (0 to 15) of std_logic_vector(23 downto 0); --declares matrix of 16 rowns with vectors of 24 bits
-   type SAMPLE_MATRIX is array (0 to 63) of std_logic_vector(23 downto 0); --declares matrix of 16 rowns with vectors of 24 bits
+   type SAMPLE_MATRIX is array (0 to 63) of std_logic_vector(23 downto 0); --declares matrix of 64 rowns with vectors of 24 bits
 end package MATRIX_TYPE;
