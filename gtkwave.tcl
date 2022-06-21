425808f7dcddd6ac9194e2d0c7a5798c4890aa43
IvarNilsson <ivar.j.nilsson@gmail.com>
2022-06-21 13:26:16 +0200

    gtkwave.tcl now opens all signals and zoomfit on startup

--- gtkwave.tcl
+++ gtkwave.tcl
@@ -1,11 +1,11 @@
 #python run.py "TEST_NAME" --gtkwave-fmt vcd --gtkwave-args=-Sgkwave.tcl --gui
-'
-set nfacts [ gtkwave::getNumFacs ]
 
-for {set i 0} {$i < $nfacts} {incr $1} {
-    set name [gtkwave::getFacName $i]
+##set nfacts [ gtkwave::getNumFacs ]
 
-    gtkwave::addSignalsFromList $name
+##for {set i 0} {$i < $nfacts} {incr $1} {
+##    set name [gtkwave::getFacName $i]
+
+##    gtkwave::addSignalsFromList $name
 
     # Analog Waveforms
     # switch -glob -- $name {
@@ -17,7 +17,27 @@ for {set i 0} {$i < $nfacts} {incr $1} {
     #         gtkwave::/Edit/Insert_Analog_Height_Extension
     #     }
     # } 
-}
+##}
 
-gtkwave::/Edit/UnHighlight_All
+##gtkwave::/Edit/UnHighlight_All
+##gtkwave::/Time/Zoom/Zoom_Full
+
+
+set nfacs [ gtkwave::getNumFacs ]
+set all_facs [list]
+for {set i 0} {$i < $nfacs } {incr i} {
+    set facname [ gtkwave::getFacName $i ]
+    lappend all_facs "$facname"
+}
+set num_added [ gtkwave::addSignalsFromList $all_facs ]
+puts "num signals added: $num_added"
+
+# zoom full
 gtkwave::/Time/Zoom/Zoom_Full
+
+# Print (save as .pdf)
+#set dumpname [ gtkwave::getDumpFileName ]
+#gtkwave::/File/Print_To_File PDF {Letter (8.5" x 11")} Minimal $dumpname.pdf
+
+gtk_window_fullscreen(GTK_WINDOW(window));
+
