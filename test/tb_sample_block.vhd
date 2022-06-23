342c5b4a78d7710b6c3017b267ca4e771ef6dda9
JacobDrotz <jacobdrotz@live.se>
2022-06-23 09:23:01 +0200

    updated testbench

--- test/tb_sample_block.vhd
+++ test/tb_sample_block.vhd
@@ -16,12 +16,14 @@ end tb_sample_block;
 architecture tb of tb_sample_block is
    constant clk_cykle : time := 10 ns; -- set the duration of one clock cycle
 
-   signal data_bitstream : std_logic;
+   signal data_bitstream : std_logic := '0';
    signal clk : std_logic := '0';
    signal reset : std_logic;
    signal send : std_logic;
    signal rd_enable : std_logic;
    signal sample_error : std_logic;
+
+   signal sim_counter : integer := 0;
 
    signal clk_count : integer := 0; -- counter for the number of clock cycles
 
@@ -43,6 +45,23 @@ begin
          clk_count <= clk_count + 1;
       end if;
    end process;
+
+
+   feed_data : process(clk)
+      begin
+         if (rising_edge(clk) and sim_counter < 5) then
+            data_bitstream <= '0';
+            sim_counter <= sim_counter +1;
+
+         elsif(rising_edge(clk) and sim_counter <10) then
+            data_bitstream <= '1';
+            sim_counter <= sim_counter +1;
+         end if;
+
+         if (sim_counter = 10) then
+         sim_counter <= 0;
+         end if;
+      end process;
 
 
 
