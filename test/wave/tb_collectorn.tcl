51fa8b2d855a9b5cfa75a73effc65f2c9cacaa4d
IvarNilsson <ivar.j.nilsson@gmail.com>
2022-06-28 14:48:59 +0200

    created tb_collectorn.tcl and small fixes to tb_sample.tcl

---
+++ test/wave/tb_collectorn.tcl
@@ -0,0 +1,22 @@
+#python ../run.py --gtkwave-fmt vcd --gui lib.tb_collectorn.tb_collectorn_1
+
+set nfacts [ gtkwave::getNumFacs ]
+puts "$nfacts"
+
+
+for {set i 2} {$i < $nfacts} {incr i} {
+    set name [gtkwave::getFacName $i]
+    puts "$name"
+
+    switch -glob -- $name {
+   
+         *clk*    -
+         tb_tb.a* {
+            puts "$name"
+            gtkwave::addSignalsFromList "$name"
+         }
+     } 
+}
+
+# zoom full
+gtkwave::/Time/Zoom/Zoom_Full
