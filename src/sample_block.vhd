5a9f306386393cc3e2ab595912ccb85c08d043d1
JacobDrotz <jacobdrotz@live.se>
2022-06-30 14:00:04 +0200

    added wrappers for componants

--- src/sample_block.vhd
+++ src/sample_block.vhd
@@ -10,66 +10,43 @@ entity sample_block is
    );
 
    port (
-      clk   : in std_logic;
+      clk : in std_logic;
+      bit_stream_v : in std_logic_vector(3 downto 0);
       reset : in std_logic;
-      bit_stream_1 : in std_logic;
-      bit_stream_2 : in std_logic;
-      bit_stream_3 : in std_logic;
-      bit_stream_4 : in std_logic;
-      sample_out_matrix : out data_out_matrix
+      ws : in std_logic;
+      sample_out_matrix : out data_out_matrix;
+      sample_error_v : out std_logic_vector(3 downto 0);
+      data_valid : out std_logic --  A signal to tell the receiver to start reading the data_out_matrix
    );
 end entity;
 
 architecture rtl of sample_block is
 
-   signal bit_stream_v : std_logic_vector(3 downto 0);
-   signal rd_enable_v : std_logic_vector(3 downto 0);
-   signal sample_error_v : std_logic_vector(3 downto 0);
-   signal send_v : std_logic_vector(3 downto 0);
-
-   component sample is
-      port(
-      bit_stream : in std_logic;
-      clk : in std_logic;
-      reset : in std_logic; -- Asynchronous reset, just nu är den inte tajmad
-      send : out std_logic;
-      rd_enable : out std_logic; 
-      sample_error : out std_logic
-      ); 
-   end component sample;
-
+   signal data_valid_v : std_logic_vector(3 downto 0);
+   signal data_out_matrix_v : std_logic_vector(3 downto 0);
 begin
-
-   bit_stream_v(0) <= bit_stream_1;
-   bit_stream_v(1) <= bit_stream_2;
-   bit_stream_v(2) <= bit_stream_3;
-   bit_stream_v(3) <= bit_stream_4;
-
-   c_gen : for i in 0 to 3 generate
+   sample_machine_gen : for i in 0 to 3 generate
    begin
-      U : sample port map(
+      sample_machines : entity work.sample_machine port map(
          bit_stream => bit_stream_v(i),
          clk => clk,
          reset => reset,
-         send => send_v(i),
-         rd_enable => rd_enable_v(i),
-         sample_error => sample_error_v(i)
+         ws => ws,
+         sample_error => sample_error_v(i),
+         data_out_matrix => data_out_matrix_v(i),
+         data_valid => data_valid_v(i)
       );
-   end generate c_gen;
-   
-   collect_sample1 : entity work.collect_sample port map(
-      data_in_1 => send_v(0),
-      data_in_2 => send_v(1),
-      data_in_3 => send_v(2),
-      data_in_4 => send_v(3),
-      rd_enable_1 => rd_enable_v(0),
-      rd_enable_2 => rd_enable_v(1),
-      rd_enable_3 => rd_enable_v(2),
-      rd_enable_4 => rd_enable_v(3),
+   end generate sample_machine_gen;
+
+   full_sample : entity work.full_sample port map(
       clk => clk,
       reset => reset,
-      sample_out_matrix => sample_out_matrix
-   );
-
-
+      sample_out_matrix => sample_out_matrix,
+      data_in_matrix_1 => data_out_matrix_v(0),
+      data_in_matrix_2 => data_out_matrix_v(1),
+      data_in_matrix_3 => data_out_matrix_v(2),
+      data_in_matrix_4 => data_out_matrix_v(3),
+      data_valid_v => data_valid_v,
+      rd_enable => data_valid
+      );
 end architecture;
---
+++ src/sample_machine.vhd
@@ -0,0 +1,48 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+use work.MATRIX_TYPE.all;
+
+entity sample_machine is
+   generic (
+      bits_mic : integer := 24;
+      nr_mics : integer := 64
+   );
+
+   port (
+      bit_stream : in std_logic;
+      clk : in std_logic;
+      reset : in std_logic; -- Asynchronous reset, just nu är den inte tajmad
+      ws : in std_logic;
+      sample_error : out std_logic := '0';
+      data_out_matrix : out MATRIX; -- Our output Matrix with 1 sample from all microphones in the Matrix
+      data_valid : out std_logic := '0' --  A signal to tell the receiver to start reading the data_out_matrix
+   );
+end sample_machine;
+
+architecture structual of sample_machine is
+
+   signal rd_enable : std_logic := '0';
+   signal reg : std_logic_vector(23 downto 0);
+
+begin
+   sample_1 : entity work.sample port map(
+      bit_stream => bit_stream,
+      clk => clk,
+      reset => reset,
+      ws => ws,
+      reg => reg,
+      rd_enable => rd_enable,
+      sample_error => sample_error
+      );
+
+   collectorn_1 : entity work.collectorn port map(
+      clk => clk,
+      reset => reset,
+      data_in => reg,
+      rd_enable => rd_enable,
+      data_valid => data_valid,
+      data_out_matrix => data_out_matrix
+      );
+
+end architecture;
