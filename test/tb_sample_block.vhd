8ea6c1f5402366b9a2f754455e629c0a0f19ceec
IvarNilsson <ivar.j.nilsson@gmail.com>
2022-06-30 13:53:16 +0200

    small fixes to full_sample and tb_sample_block

--- test/tb_sample_block.vhd
+++ test/tb_sample_block.vhd
@@ -14,17 +14,15 @@ entity tb_sample_block is
 end tb_sample_block;
 
 architecture tb of tb_sample_block is
-   constant clk_cykle : time := 10 ns; -- set the duration of one clock cycle
-
-   signal bit_stream_1 : std_logic := '0';
-   signal bit_stream_2 : std_logic := '0';
-   signal bit_stream_3 : std_logic := '0';
-   signal bit_stream_4 : std_logic := '0';
    signal clk : std_logic := '0';
    signal reset : std_logic := '0';
-   signal sample_out_matrix : data_out_matrix;
+   signal ws : std_logic := '0';
 
-   signal sim_counter : integer := 0;
+   signal bit_stream_v : std_logic_vector(3 downto 0) := "0000";
+
+   signal sample_out_matrix : data_out_matrix;
+   signal data_valid : std_logic;
+   signal sample_error_v : std_logic_vector (3 downto 0);
 
    signal matrix_row_0 : std_logic_vector(23 downto 0); -- slinga 0, mic 0
    signal matrix_row_1 : std_logic_vector(23 downto 0); -- slinga 0, mic 1
@@ -39,7 +37,9 @@ architecture tb of tb_sample_block is
    signal matrix_row_49 : std_logic_vector(23 downto 0); -- slinga 3, mic 1
    signal matrix_row_63 : std_logic_vector(23 downto 0); -- slinga 3, mic 15
 
+   constant clk_cykle : time := 10 ns; -- set the duration of one clock cycle
    signal clk_count : integer := 0; -- counter for the number of clock cycles
+   signal sim_counter : integer := 0;
 
    procedure clk_wait (nr_of_cykles : in integer) is
    begin
@@ -49,6 +49,18 @@ architecture tb of tb_sample_block is
    end procedure;
 
 begin
+
+   sample_block1 : entity work.sample_block port map (
+      clk => clk,
+      reset => reset,
+      ws => ws,
+
+      bit_stream_v => bit_stream_v,
+
+      sample_out_matrix => sample_out_matrix,
+      data_valid => data_valid,
+      sample_error_v => sample_error_v
+      );
 
    matrix_row_0 <= sample_out_matrix(0);
    matrix_row_1 <= sample_out_matrix(1);
@@ -63,16 +75,6 @@ begin
    matrix_row_49 <= sample_out_matrix(49);
    matrix_row_63 <= sample_out_matrix(63);
 
-   sample_block1 : entity work.sample_block port map (
-      bit_stream_1 => bit_stream_1,
-      bit_stream_2 => bit_stream_2,
-      bit_stream_3 => bit_stream_3,
-      bit_stream_4 => bit_stream_4,
-      clk => clk,
-      reset => reset,
-      sample_out_matrix => sample_out_matrix
-      );
-
    -- counter for clk cykles
    clk_counter : process (clk)
    begin
@@ -84,19 +86,11 @@ begin
    feed_data : process (clk)
    begin
       if (rising_edge(clk) and sim_counter < 5) then
-         bit_stream_1 <= '0';
-         bit_stream_2 <= '0';
-         bit_stream_3 <= '0';
-         bit_stream_4 <= '0';
-
+         bit_stream_v <= "0000";
          sim_counter <= sim_counter + 1;
 
       elsif (rising_edge(clk) and sim_counter < 10) then
-         bit_stream_1 <= '1';
-         bit_stream_2 <= '1';
-         bit_stream_3 <= '1';
-         bit_stream_4 <= '1';
-
+         bit_stream_v <= "0000";
          sim_counter <= sim_counter + 1;
       end if;
 
@@ -124,8 +118,6 @@ begin
             wait for 30000 ns;
             check(1 = 1, "test_1");
          elsif run("tb_sample_block_2") then -- test 2, automatic checks after verius intervals
-
-
             check(1 = 1, "test_1");
             wait for 11 ns;
 
