6093cd05a3df00edc521b2570999bba79198dba1
IvarNilsson <ivar.j.nilsson@gmail.com>
2022-07-20 12:39:04 +0200

    rd_en_pulse added (tb not working yet)

--- pl/run.py
+++ pl/run.py
@@ -12,6 +12,7 @@ lib.add_source_files(ROOT.glob("test/**/*.vhd"))
 lib.add_source_files(ROOT.glob("src/sample_data/*.vhd"))
 lib.add_source_files(ROOT.glob("src/ws_pulse/*.vhd"))
 lib.add_source_files(ROOT.glob("src/demo_count.vhd"))
+lib.add_source_files(ROOT.glob("src/rd_en_pulse.vhd"))
 lib.add_source_files(ROOT.glob("src/matrix_package.vhd"))
 
 for l in lib.get_test_benches():
