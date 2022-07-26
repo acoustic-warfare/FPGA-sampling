9970c4c63adc050402f48b8c2b31c7ccfcd89521
IvarNilsson <ivar.j.nilsson@gmail.com>
2022-07-26 12:02:32 +0200

    udp:p sorting bytes right :)

--- ps/udp_p.py
+++ ps/udp_p.py
@@ -1,48 +1,58 @@
 import socket
 import sys
 
-print("test 1")
+print("START")
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 21844
 
-print("test 1")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print("test 2")
+
 sock.bind((UDP_IP, UDP_PORT))
-print("test 3")
+
 
 print("Socket: "+str(sock.getsockname()))
-print("test 4")
 
 
-i=0
-counter = 0
+def twos_comp(val, bits):
+    if(val & (1 << (bits - 1))) != 0:
+        val = val - (1 << bits)
+    return val
+
+
 while 1:
 
-   data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
 
-      
-   #str_data = data.decode('UTF_16')
+    # print(" recived: ", data.decode('utf-8'), "\n")
+    test0 = "{0:b}".format(data[0])
+    test1 = "{0:b}".format(data[1])
+    test2 = "{0:b}".format(data[2])
+    test3 = "{0:b}".format(data[3])
 
-  # with open("data.txt",'wb') as binary_file:
-      # udp_data = ['udp.txt',data]
-     # binary_file.write(data)
-      
-      
-   counter = counter +1
+    test4 = "{0:b}".format(data[4])
+    test5 = "{0:b}".format(data[5])
+    test6 = "{0:b}".format(data[6])
+    test7 = "{0:b}".format(data[7])
 
-   print(counter)
-  # print(data) 
-   #print(addr)
-   sys.stdout.flush()
+    test00 = test0.zfill(8)
+    test11 = test1.zfill(8)
+    test22 = test2.zfill(8)
+    test33 = test3.zfill(8)
 
-  # binary_file.close()
+    test44 = test4.zfill(8)
+    test55 = test5.zfill(8)
+    test66 = test6.zfill(8)
+    test77 = test7.zfill(8)
 
-   #print("opening file....")
+    test_full = test33 + test22 + test11 + test00
+    test_full2 = test77 + test66 + test55 + test44
+    #print("test_full: ", test_full)
 
-   #with open("data.txt", "rb") as binary_file:
-    #  data_out = binary_file.read()
-    #  print(data_out)
-  # i = i +1
+    out = twos_comp(int(test_full, 2), len(test_full))
+    out2 = twos_comp(int(test_full2, 2), len(test_full2))
+    print("1: ", out)
+    print("2: ", out2)
+
+    sys.stdout.flush()
