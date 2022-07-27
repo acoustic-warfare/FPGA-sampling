f924e715d6fc8e12ae2811b4f8ec07bbf13a6467
IvarNilsson <ivar.j.nilsson@gmail.com>
2022-07-27 12:49:26 +0200

    created sound_player to play the recorded sound

---
+++ ps/sound_player.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sounddevice
+filename= '/home/ljudkriget/Projects/Ljud_Kriget/ps/data.txt'
+fs = 48828
+data = np.loadtxt(open(filename,'r'))
+print(np.shape(data))
+print(data)
+
+scaled_data = data/np.max(np.abs(data))
+sounddevice.play(scaled_data, fs, blocking=True)
+
