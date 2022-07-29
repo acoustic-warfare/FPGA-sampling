4968141f632144c4953de95d8e237e6714dfc77a
IvarNilsson <ivar.j.nilsson@gmail.com>
2022-07-29 16:52:42 +0200

    more updates to play sound from 64 mics

--- ps/sound_player.py
+++ ps/sound_player.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice
-filename= '/home/ljudkriget/Projects/ljud_kriget/ps/tetxc5.txt'
-fs = 19531.25
-data = np.loadtxt(open(filename,'r'))
+filename= '/home/ljudkriget/Projects/Ljud_Kriget/ps/nanana.txt'
+mic = 1
+fs = 15625
+data = np.loadtxt(open(filename,'rb'), delimiter=',')
 print(np.shape(data))
 print(data)
 
-scaled_data = data/np.max(np.abs(data))
+scaled_data = data/np.max(np.abs(data[:,mic]))
 sounddevice.play(scaled_data, fs, blocking=True)
+
 
--- ps/udp_c.c
+++ ps/udp_c.c
@@ -1,83 +1,65 @@
-// Server side implementation of UDP client-server model 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-    
-#define PORT     21844 
-#define MAXLINE 1024 
-    
-// Driver code 
-int main() { 
-    
-      int sockfd; 
-      int buffer[MAXLINE]; 
-      
-      struct sockaddr_in servaddr, cliaddr; 
-         
-      // Creating socket file descriptor 
-      if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-         perror("socket creation failed"); 
-         exit(EXIT_FAILURE); 
-      } 
-         
-      memset(&servaddr, 0, sizeof(servaddr)); 
-      memset(&cliaddr, 0, sizeof(cliaddr)); 
-         
-      // Filling server information 
-      servaddr.sin_family    = AF_INET; // IPv4 
-      servaddr.sin_addr.s_addr = INADDR_ANY; 
-      servaddr.sin_port = htons(PORT); 
-         
-      // Bind the socket with the server address 
-      if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
-               sizeof(servaddr)) < 0 ) 
-      { 
-         perror("bind failed"); 
-         exit(EXIT_FAILURE); 
-      } 
+// Server side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
+#define PORT 21844
+#define MAXLINE 1024
 
-      FILE *fp =fopen("tetxc5.txt","w");
-      while(1){   
-      int len, n; 
-      
-      len = sizeof(cliaddr);  //len is value/result 
-      
-      n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
-                  MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
-                  &len); 
-      buffer[n] = '\0'; 
+// Driver code
+int main()
+{
 
+   int sockfd;
+   int buffer[MAXLINE];
 
-      /*for(int i =0; i<10; i++) {
-         if(buffer[i] > 65279){
-            buffer[i] = 0;
-         }
-         
-         
-         else if (buffer[i] < -65279) {
-            buffer[i] = 0;
-         }
-      }*/
-      //printf(" %d \n", buffer[0]); 
-      fprintf(fp, "%d \n", buffer[50]);
-      //fprintf(fp, "%d \n", buffer[1]);
-      //fprintf(fp, "%d \n", buffer[2]);
-      //fprintf(fp, "%d \n", buffer[3]);
-      //fprintf(fp, "%d \n", buffer[4]);
-      //fprintf(fp, "%d \n", buffer[5]);
-      //fprintf(fp, "%d \n", buffer[6]);
-      //fprintf(fp, "%d \n", buffer[7]);
-      //fprintf(fp, "%d \n", buffer[8]);
-      //fprintf(fp, "%d \n", buffer[9]);
-      
+   struct sockaddr_in servaddr, cliaddr;
 
-      
-    }     
-    return 0; 
+   // Creating socket file descriptor
+   if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+   {
+      perror("socket creation failed");
+      exit(EXIT_FAILURE);
+   }
+
+   memset(&servaddr, 0, sizeof(servaddr));
+   memset(&cliaddr, 0, sizeof(cliaddr));
+
+   // Filling server information
+   servaddr.sin_family = AF_INET; // IPv4
+   servaddr.sin_addr.s_addr = INADDR_ANY;
+   servaddr.sin_port = htons(PORT);
+
+   // Bind the socket with the server address
+   if (bind(sockfd, (const struct sockaddr *)&servaddr,
+            sizeof(servaddr)) < 0)
+   {
+      perror("bind failed");
+      exit(EXIT_FAILURE);
+   }
+
+   FILE *fp = fopen("nanana.txt", "w");
+   while (1)
+   {
+      int len, n;
+
+      len = sizeof(cliaddr); // len is value/result
+
+      n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                   MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                   &len);
+      buffer[n] = '\0';
+
+      for (int i = 0; i < 63; i++)
+      {
+         fprintf(fp, "%d, ", buffer[i]);
+      }
+      fprintf(fp, "%d\n", buffer[63]);
+   }
+   return 0;
 }
