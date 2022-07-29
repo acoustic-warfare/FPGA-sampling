51f97835bbd335062e2b50deffd15b76b86a48e6
IvarNilsson <ivar.j.nilsson@gmail.com>
2022-07-29 09:02:16 +0200

    rd_en_pulse update

--- pl/src/rd_en_pulse.vhd
+++ pl/src/rd_en_pulse.vhd
@@ -4,10 +4,10 @@ use ieee.numeric_std.all;
 
 entity rd_en_pulse is
    port (
-      clk_axi         : in std_logic;
-      reset           : in std_logic;
-      rd_en_array_in  : in std_logic;
-      rd_en_array_out : out std_logic
+      clk_axi   : in std_logic;
+      reset     : in std_logic;
+      rd_en_in  : in std_logic;
+      rd_en_out : out std_logic
    );
 end rd_en_pulse;
 
@@ -18,19 +18,19 @@ begin
    process (clk_axi)
    begin
       if (rising_edge(clk_axi)) then
-         rd_en_array_out <= '0';
-         if (rd_en_array_in = '1' and active = '0') then
-            rd_en_array_out <= '1';
-            active          <= '1';
+         rd_en_out <= '0';
+         if (rd_en_in = '1' and active = '0') then
+            rd_en_out <= '1';
+            active    <= '1';
          end if;
 
-         if (rd_en_array_in = '0') then
+         if (rd_en_in = '0') then
             active <= '0';
          end if;
 
          if (reset = '1') then
-            rd_en_array_out <= '0';
-            active          <= '0';
+            rd_en_out <= '0';
+            active    <= '0';
          end if;
       end if;
    end process;
