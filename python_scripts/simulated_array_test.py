31712a71eacf310d021a1ab43287bb63983a8eb7
IvarNilsson <ivar.j.nilsson@gmail.com>
2023-07-12 07:01:33 +0200

    for test

--- python_scripts/simulated_array_test.py
+++ python_scripts/simulated_array_test.py
@@ -13,7 +13,7 @@ ROOT = os.getcwd()
 print("Enter name of file to analyze: ")
 file=input()
 
-print("Simulated data from a FPGA? [y/n]")
+print("Simulated data from same FPGA? [y/n]")
 if(input().lower() == "y"):
     simulated = True
 else:   
@@ -25,22 +25,23 @@ sampelCounter_index = 7
 sampleCounterPS = [] #fixa något snyggt här som spara datan tror jab blir bäst
 sampleCounterPL = []
 line_index = 0
+wrong = 1
+line_count = 1
+
 with open(file+".txt") as f:
     lines = [line for line in f]
 
-    line_count = 0
 
     for line in lines:
        line_count += 1
        mic_values = line.split()
-       sampleCounterPS.append(int(mic_values[sampelCounter_index]))
        if(simulated):
-          for i in range(1,64):
-             if(mic_values[header_length + 3] != mic_values[header_length + 3 + 4 * i]):
-                 print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                 print("index: " + i + " not matching! at sampleCounter: " + mic_values[sampelCounter_index] + " at line: " + line_index + " in the txt file")
-                 print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-          sampleCounterPL.append(int(mic_values[11], 2))
+          if(mic_values[71] != mic_values[72]):
+             wrong += 1
+
+print("antal samples: " + str(line_count))
+print("antal fel: " + str(wrong))
+print("fel: " + "%.3f" % (wrong / line_count * 100) + "%")
 
 
 
@@ -48,24 +49,7 @@ with open(file+".txt") as f:
 
 
 
-print(line_count)
-print(len(sampleCounterPS))
 
-for i in range(1, line_count):
-    if(simulated):
-       if(sampleCounterPL[i - 1] + 1 != sampleCounterPL[i] and sampleCounterPS[i - 1] + 1 != sampleCounterPS[i]):
-           print("Problem1 at line: " + str(i) + ", " + str(i+1) + " in the txt file, jump: " + str(abs(sampleCounterPS[i - 1] - sampleCounterPS[i])) + " places")
-       elif(sampleCounterPL[i - 1] + 1 != sampleCounterPL[i]):
-           print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-           print("Problem2 : PL_counter = " + str(sampleCounterPL[i]) + " at line: " + str(i) + ", " + str(i+1) + " in the txt file")
-           print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-       elif(sampleCounterPS[i - 1] + 1 != sampleCounterPS[i]):
-           print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-           print("Problem3 : PS_counter = " + str(sampleCounterPS[i]) + " at line: " + str(i) + ", " + str(i+1) + " in the txt file")
-           print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    else: 
-        if(sampleCounterPS[i - 1] + 1 != sampleCounterPS[i]):
-            print("Problem1 at line: " + str(i) + ", " + str(i+1) + " in the txt file, jump: " + str(abs(sampleCounterPS[i - 1] - sampleCounterPS[i])) + " places")
 
 
 f.close
