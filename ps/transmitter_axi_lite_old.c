ab42edbf16fce74ecd250ea68ad06c027fa41bb9
Tuva Björnberg <tuva.bjornberg@saabgroup.com>
2023-07-25 10:40:31 +0200

    Now axi lite is also up to date (just in case)

--- ps/transmitter_axi_lite_old.c
+++ ps/transmitter_axi_lite_old.c
@@ -57,7 +57,7 @@ int main() {
 
     u16_t Port = 21844;
 
-    int buflen = 500;
+    int buflen = 1458;
 
     /* The MAC address of the board. this should be unique per board */
     unsigned char mac_ethernet_address[] = {0x00, 0x00, 0x00, 0x01, 0x00, 0x00};
