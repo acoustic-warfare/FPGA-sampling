a2ff225ec279c7158ec176d041c631ae9902b86c
Tuva <tuva.bjornberg02@gmail.com>
2024-06-19 09:18:55 +0200

    build runs docker

--- pl/scripts/other/build.sh
+++ pl/scripts/other/build.sh
@@ -17,6 +17,8 @@ ELF_PATH="$GIT_ROOT/pl/vivado_files/acoustic_warfare.sdk/aw_udp_ps/Debug/aw_udp_
 
 # Destination tftp directory
 DEST_DIR="$GIT_ROOT/../beamforming-lk/boot/tftp"
+DOCKERFILE_DIR="$GIT_ROOT/../beamforming-lk/boot"
+TFTP_DIR="$GIT_ROOT/../beamforming-lk/boot/tftp"
 # END PATHS
 
 # FLAGS
@@ -57,13 +59,32 @@ if [ "$skip_sdk" = false ]; then
    source /tools/Xilinx/SDK/2017.4/settings64.sh
    if [ -d "$GIT_ROOT/pl/vivado_files/acoustic_warfare.sdk" ]; then
       rm -r "$GIT_ROOT/pl/vivado_files/acoustic_warfare.sdk" # remove folder if it exists
-   fi   
+   fi
    xsct $GIT_ROOT/pl/scripts/build_axi_full/launch_sdk.tcl
 fi
 
 printf "%b\n" "${BOLD}copying files...${RESET}"
 cp "$BITSTREAM_PATH" "$DEST_DIR/bitstream"
 cp "$ELF_PATH" "$DEST_DIR/ps.elf"
+
+IMAGE_NAME="tftpd"
+CONTAINER_NAME="tftp-server"
+
+# Check if the Docker image exists
+if [ -z "$(sudo docker images -q $IMAGE_NAME)" ]; then
+   echo "Docker image $IMAGE_NAME does not exist. Building the image..."
+   sudo docker build -t $IMAGE_NAME -f $DOCKERFILE_DIR/Dockerfile .
+else
+   echo "Docker image $IMAGE_NAME already exists. Skipping build."
+fi
+
+# Check if the Docker container is running
+if sudo docker ps --filter "name=$CONTAINER_NAME" --format "{{.Names}}" | grep -w $CONTAINER_NAME >/dev/null; then
+   echo "Docker container $CONTAINER_NAME is already running."
+else
+   echo "Docker container $CONTAINER_NAME is not running. Starting the container..."
+   sudo docker run -it -d --rm --network=host -v $TFTP_DIR:/var/lib/tftpboot --name $CONTAINER_NAME $IMAGE_NAME
+fi
 
 printf "%b\n" "${BOLD}${GREEN}done!${RESET}"
 exit 0
